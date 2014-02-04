a832ec68143c64368542b51c52953ef0c407ed95
ehirao14 <ehirao14@punahou.edu>
2014-02-03 14:23:36 -1000

    I made a new board that is based on the array size. I also made 3 new players.

--- FortheGloryofKiang.java
+++ FortheGloryofKiang.java
@@ -2,4 +2,25 @@ public class FortheGloryofKiang
 {
   public static void main(String[] args)
   {
-  public String[][] ticTacArray = new String[3][3];
+    String[][] KiangArray = new String[3][9];
+    int player1 = 1; //makes 3 ints to be placemakers for the games for 3 players
+    int player2 = 2;
+    int player3 = 3;
+    Table(KiangArray);
+  }
+  public static void Table(String[][] array)
+  {
+    System.out.println("Tic-Tac-Toe game");
+    for(int row = 0; row < array.length; row++) //goes through the row after making columns
+    {
+      for(int column = 0; column < array[row].length; column++){
+        System.out.print("| |"); // will print down 3 before changing rows
+        if(column%3-2==0)
+        {
+          System.out.print("        "); //makes 3 separate plots to make a 2d rendering of the 3d game
+        }
+      }
+      System.out.println("");// print new line so we can start a new column
+    }
+  }
+}
