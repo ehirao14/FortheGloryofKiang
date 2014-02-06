c462584aa2f2a429f95581de629344791ddf3a93
ehirao14 <ehirao14@punahou.edu>
2014-02-05 14:26:15 -1000

    Rethinking my life
    Had a long talk with Malcolm. (AKA the lord of our class.)
    
    I'm not sure if I should make the 3d TTT have gravity or not because I
    would need to make a 3d array. Right now I'm trying to code it was a 2d
    array. I also realized that I need to make a while statement since the
    main method would just erase all my progress. I also realized that I
    need to check if someone won, meaning that I would need to check every
    single possibility and code that.

--- FortheGloryofKiang.java
+++ FortheGloryofKiang.java
@@ -6,7 +6,15 @@ public class FortheGloryofKiang
     int player1 = 1; //makes 3 ints to be placemakers for the games for 3 players
     int player2 = 2;
     int player3 = 3;
-    Table(KiangArray);
+    
+    boolean gameOver = false;
+    
+    while(!gameOver)
+    {
+      //check if any possibilty that someone won (this is be ginormous)
+      Table(KiangArray);
+      //let first player go
+    }
   }
   public static void Table(String[][] array)
   {
@@ -15,12 +23,17 @@ public class FortheGloryofKiang
     {
       for(int column = 0; column < array[row].length; column++){
         System.out.print("| |"); // will print down 3 before changing rows
-        if(column%3-2==0)
+        if(column%3-2==0) //jesus this took a lot of guessing
         {
           System.out.print("        "); //makes 3 separate plots to make a 2d rendering of the 3d game
         }
       }
       System.out.println("");// print new line so we can start a new column
     }
+  }
+  public static void player1()
+  {
+    KiangArray[2][6] = "1";
+    Table(KiangArray);
   }
 }
